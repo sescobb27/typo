89c246617981290d9096da76f6fd6cd47828c37f
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-08-23 13:54:40 -0500

    new_or_edit categories for feature

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -24,9 +24,21 @@ class Admin::CategoriesController < Admin::BaseController
   private
 
   def new_or_edit
+    id = params[:id]
+    if params[:category] && params[:category][:id]
+      id = params[:category][:id]
+    end
+
+    if id   # edit
+      @category = Category.find(id)
+    else    # create
+      @category = Category.new
+    end
+
+    if params[:category]
+      @category.attributes = params[:category]
+    end 
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
-    @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
         format.html { save_category }
